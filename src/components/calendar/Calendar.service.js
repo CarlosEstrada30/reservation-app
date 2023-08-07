8d84f1a00b19100ef52972cef591fd4aff975830
Carlos Humberto Estrada Godoy <carlos.estrada@rankmi.com>
2023-08-07 07:36:04 -0600

    Fix calendar service

--- src/components/calendar/Calendar.service.js
+++ src/components/calendar/Calendar.service.js
@@ -4,23 +4,15 @@ import  handleResponse  from '../../providers'
 const api = process.env.REACT_APP_API_URL+'/api/v1/event'
 
 const getEvents = () =>
-  axiosInstance.get(api, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    }
-  }).then(handleResponse)
+  axiosInstance.get(api).then(handleResponse)
 
 const update_or_create = (data) =>
   axiosInstance
-    .post(api, data, {
-      withCredentials: true
-    })
+    .post(api, data)
     .then(handleResponse)
 
 const del = (token) =>
-  axiosInstance.delete(`${api}/${token}`, {
-    withCredentials: true
-  }).then(handleResponse)
+  axiosInstance.delete(`${api}/${token}`).then(handleResponse)
 
 
 export const calendarService = {
