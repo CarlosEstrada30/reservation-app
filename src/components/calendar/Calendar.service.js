a2f3fc2a5947952d78d37b0a31ed94235ba25bf4
Carlos Humberto Estrada Godoy <carlos.estrada@rankmi.com>
2023-09-09 10:29:20 -0600

    change api url to enviroment

--- src/components/calendar/Calendar.service.js
+++ src/components/calendar/Calendar.service.js
@@ -1,8 +1,8 @@
 import axiosInstance  from '../../providers/axios.provider'
 import  handleResponse  from '../../providers'
 
-// const api = process.env.REACT_APP_API_URL+'/api/v1/event'
-const api = 'https://3d37-190-148-128-190.ngrok-free.app/api/v1/event'
+const api = process.env.REACT_APP_API_URL+'/api/v1/event'
+// const api = 'https://3d37-190-148-128-190.ngrok-free.app/api/v1/event'
 
 const getEvents = () =>
   axiosInstance.get(api).then(handleResponse)
