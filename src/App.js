83f2b37904616c5a69498885682eedcfe1c11d6e
Carlos Humberto Estrada Godoy <carlos.estrada@rankmi.com>
2023-09-08 21:22:51 -0600

    delete unused imports

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from "react";
 import { ProtectedRoute } from "./components/protectedRoute/ProtectedRoute";
 import LoginPage from './pages/Login'
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-// import { ProtectedRoute } from "./components/protectedRoute/ProtectedRoute";
-// import CalendarPage from "./pages/Calendar";
 import PublicCalendar from "./pages/PublicCalendar";
-import NewReservation from "./pages/NewReservation";
-// import LoginPage from "./pages/Login";
 import CustomAppBar from "./components/appBar/AppBar";
 
-// import "./App.css";
 
 function App() {
   const isLogin = localStorage.getItem("reservation_jwt") ? true : false;
